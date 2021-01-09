e5b16f98e5c7caceeec81bc5b6cecba22386410d
kwkwc <wei497896143@yeah.net>
2021-01-09 08:11:19 +0000

    feat: remove redundant operations

--- init.sh
+++ init.sh
@@ -3,10 +3,10 @@
 # Program:
 #      初始化
 # Version:
-#      1.0.2
+#      1.0.3
 # History:
 #      Created on 2018/07/16
-#      Last modified on 2019/10/13
+#      Last modified on 2021/01/09
 # Author:
 #      kwkw
 
@@ -25,22 +25,6 @@ then
     SUDO="sudo"
 else
     SUDO=""
-fi
-
-# Ubuntu 16.04
-if [ "$DISTRO" = "Ubuntu" ]
-then
-    :
-    # 备份源
-    # $SUDO mv /etc/apt/sources.list /etc/apt/sources.list.bak$(date -d "today" +"%Y%m%d_%H%M%S")
-
-    # 替换源
-    # $SUDO sh -c 'echo "deb https://mirrors.ustc.edu.cn/ubuntu/ xenial main restricted universe multiverse\n\
-    # deb https://mirrors.ustc.edu.cn/ubuntu/ xenial-security main restricted universe multiverse\n\
-    # deb https://mirrors.ustc.edu.cn/ubuntu/ xenial-updates main restricted universe multiverse\n\
-    # deb https://mirrors.ustc.edu.cn/ubuntu/ xenial-proposed main restricted universe multiverse\n\
-    # deb https://mirrors.ustc.edu.cn/ubuntu/ xenial-backports main restricted universe multiverse" >> /etc/apt/sources.list' && \
-    # $SUDO $PM update
 fi
 
 # 安装 docker
